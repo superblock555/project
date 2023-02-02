9497f21bf22144b9108446861bb448eb7fe51ee9
Superblock Minecraft <mattvey0311@gmail.com>
2023-02-02 10:11:44 +0300

    ass

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PodborComponent } from './podbor/podbor.component';
+import { VuzComponent } from './vuz/vuz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  { path: 'home', component: HomeComponent },
+  { path: 'podbor', component: PodborComponent },
+  { path: 'vuz', component: VuzComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
+  isLogged: boolean = false;
+  accountUsername: string = '';
+
+  constructor() {
+    this.checkUser();
+  }
+
+  checkUser(): void {
+    let user_str = localStorage.getItem('userinfo');
+    if (user_str === null) {
+      return;
+    } else {
+      let user = JSON.parse(user_str);
+      this.accountUsername = user.username;
+      this.isLogged = true;
+    }
+  }
+
+  logout(): void {
+    localStorage.clear()
+    this.accountUsername = '';
+    this.isLogged = false;
+  }
 }
